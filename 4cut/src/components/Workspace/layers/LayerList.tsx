948f22eb00c8919878d1078d72235522baaf503b
jisu-d <lim16111611@gmail.com>
2025-06-16 03:06:16 +0900

    레이어 기능 거의 완성

--- 4cut/src/components/Workspace/layers/LayerList.tsx
+++ 4cut/src/components/Workspace/layers/LayerList.tsx
@@ -1,13 +1,44 @@
-// src/components/LayerItem.js
-import React from 'react';
-
 import '../../../styles/Workspace/layers/LayerList.css'
 
-function LayerList() {
-//   const { id, name, isVisible } = layer;
+import addIcon from '../../../assets/Icon/add.svg'
+import type { ListItem } from '../../../types/types'
+
+import DndList from './DndList'
+import React, { useContext, useState } from 'react';
+
+import AppContext from '../../../contexts/AppContext';
+
+const LayerList = () => {
+  const context = useContext(AppContext);
+
+  if (!context.layer) {
+    return <div>레이어 데이터를 불러오는 중...</div>;
+  }
+  const { layerData, setLayerData } = context.layer.layerData;
+
+  const handleAddLayer = () => {
+    console.log(layerData.length + 1);
+    
+    const newLayer: ListItem = {
+      id: String(layerData.length + 1),
+      text: `새 레이어 ${layerData.length + 1}`, // 예시 텍스트
+      checked: true,
+    };
+
+    setLayerData((prevLayerData: ListItem[]) => [...prevLayerData, newLayer]);
+  };
 
   return (
-    <div className='layer-list-section'>dd</div>
+    <div className='layer-control-section'>
+      <div>
+        <div>레이어 변경</div>
+        <img src={addIcon} onClick={handleAddLayer} alt="" />
+      </div>
+      <DndList
+        items={layerData}
+        setItems={setLayerData}
+      />
+    </div>
   );
 }
 
