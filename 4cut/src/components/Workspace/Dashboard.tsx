ca8a48b713e877f0adb8a2bbf67cb0a8fa6b4450
jisu-d <lim16111611@gmail.com>
2025-06-15 21:46:58 +0900

    건우형 commit함요

--- 4cut/src/components/Workspace/Dashboard.tsx
+++ 4cut/src/components/Workspace/Dashboard.tsx
@@ -1,55 +1,86 @@
 // src/components/Dashboard.js
 import React, { useState } from 'react';
 
-import '../../styles/Workspace/Dashboard.css'; 
+import '../../styles/Workspace/Dashboard.css';
 
 import imgIcon from '../../assets/Icon/img.svg';
 import uploadIcon from '../../assets/Icon/upload.svg';
+
 import brushIcon from '../../assets/Icon/brush.svg';
+import brushIconOn from '../../assets/Icon/brushOn.svg';
+
 import layerIcon from '../../assets/Icon/layer.svg';
+import layerIconOn from '../../assets/Icon/layerOn.svg';
+
+import LayersContent from './LayersContent';
+
+const PreviewContent = () => <div>Add Image Content</div>;
+const SettingsContent = () => <div>Export Content</div>;
+const TemplateSelectionContent = () => <div>Brushes Content</div>;
+const ExportContent = () => <div>Layers/Colors Content</div>;
+
 
 function Dashboard() {
-  // 현재 선택된 메뉴를 나타내는 상태
-  const [selectedMenu, setSelectedMenu] = useState('preview'); // 초기값은 'preview'
+  const [selectedMenu, setSelectedMenu] = useState('layers'); // 초기값
 
-  // 선택된 메뉴에 따라 오른쪽에 렌더링할 컴포넌트를 결정하는 함수
-//   const renderContent = () => {
-//     switch (selectedMenu) {
-//       case 'preview':
-//         return <PreviewContent />;
-//       case 'settings':
-//         return <SettingsContent />;
-//       case 'templates':
-//         return <TemplateSelectionContent />;
-//       case 'export':
-//         return <ExportContent />;
-//       default:
-//         return <PreviewContent />; // 기본값 또는 에러 처리
-//     }
-//   };
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case 'addImg':
+        return <PreviewContent />;
+      case 'export':
+        return <SettingsContent />;
+      case 'brushes':
+        return <TemplateSelectionContent />;
+      case 'layers':
+        return <LayersContent />;
+      case 'colors':
+        return <ExportContent />;
+      default:
+        return (<div>Error: Content not found.</div>); // 기본값 또는 에러 처리
+    }
+  };
+
+  // 조건 ? 참일 때의 값 : 거짓일 때의 값
 
   return (
-    // <div className="dashboard-container">
-    //   <Sidebar setSelectedMenu={setSelectedMenu} currentMenu={selectedMenu} /> {/* 사이드바에 상태 변경 함수 전달 */}
-    //   <main className="content-area">
-    //     {renderContent()} {/* 선택된 메뉴에 따른 컨텐츠 렌더링 */}
-    //   </main>
-    // </div>
     <>
-        <div className='tool-header'>
-            <div>
-                <img src={imgIcon} alt="img" className="img-icon-img" />
-                <img src={uploadIcon} alt="upload" className="upload-icon-img" />
-            </div>
-            <div>
-                <img src={brushIcon} alt="brush" className="brush-icon-img" />
-                <img src={layerIcon} alt="layer" className="layer-icon-img"/>
-                <div className='color-icon'></div>
-            </div>
+      <div className='tool-header'>
+        <div>
+          <span
+            className={selectedMenu === 'addImg' ? 'selected' : ''} 
+            onClick={() => setSelectedMenu('addImg')}
+          >
+            <img src={imgIcon} alt="img" className="img-icon-img" />
+          </span>
+          <span
+            className={selectedMenu === 'export' ? 'selected' : ''}
+            onClick={() => setSelectedMenu('export')}
+          >
+            <img src={uploadIcon} alt="upload" className="upload-icon-img" />
+          </span>
         </div>
-        <div className='tool-content'>
-            
+        <div>
+          <img
+            src={selectedMenu === 'brushes' ? brushIconOn : brushIcon}
+            alt="brush"
+            className="brush-icon-img"
+            onClick={() => setSelectedMenu('brushes')}
+          />
+          <img
+            src={selectedMenu === 'layers' ? layerIconOn : layerIcon}
+            alt="layer"
+            className="layer-icon-img"
+            onClick={() => setSelectedMenu('layers')}
+          />
+          <div
+            className={`color-icon ${selectedMenu === 'colors' ? 'selected' : ''}`}
+            onClick={() => setSelectedMenu('colors')}
+          ></div>
         </div>
+      </div>
+      <div className='tool-content'>
+        {renderContent()}
+      </div>
     </>
   );
 }
