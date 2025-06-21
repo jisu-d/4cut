a06cdc2ba66eee6717d76946150b9966c4789783
jisu-d <lim16111611@gmail.com>
2025-06-22 00:41:47 +0900

    export(css 수정, 스켈래톤 디자인추가), brushes 추가

---
+++ 4cut/src/components/Workspace/ExportingSkeleton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../../styles/Workspace/ExportingSkeleton.css';
+
+const ExportingSkeleton = () => {
+  return (
+    <div className="skeleton-container">
+      <div className="skeleton-header">
+        <div className="skeleton-item skeleton-title"></div>
+        <div className="skeleton-item skeleton-switch"></div>
+      </div>
+      <div className="skeleton-inputs">
+        <div className="skeleton-input-row">
+          <div className="skeleton-item skeleton-input"></div>
+          <div className="skeleton-item skeleton-input"></div>
+        </div>
+        <div className="skeleton-input-row" style={{width: "calc(50% - 8px)"}}>
+            <div className="skeleton-item skeleton-input"></div>
+        </div>
+        <div className="skeleton-item skeleton-textarea"></div>
+        <div className="skeleton-item skeleton-agreements"></div>
+      </div>
+      <div className="skeleton-button-wrapper">
+        <div className="skeleton-item skeleton-button"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ExportingSkeleton; 
