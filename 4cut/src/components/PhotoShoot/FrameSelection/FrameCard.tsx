7d3ceb3c5ebf28bfdb28c358330dd1fd00655fdd
jisu-d <lim16111611@gmail.com>
2025-09-04 00:28:43 +0900

    프레임 선택 페이지 제작

---
+++ 4cut/src/components/PhotoShoot/FrameSelection/FrameCard.tsx
@@ -0,0 +1,59 @@
+import { CustomCheckbox } from '../../CustomCheckBox.tsx';
+import '../../../styles/PhotoShoot/FrameSelection/FrameCard.css';
+
+import eye_icon from '../../../assets/Icon/Mypage/eye.svg'
+
+interface ArtworkCardProps {
+    id: string;
+    isSelected: boolean;
+    onClick: () => void;
+    title: string;
+    imageUrl: string;
+    viewCount: string;
+    createdDate: string;
+}
+
+export function ArtworkCard({ title, imageUrl, viewCount, createdDate, isSelected, onClick }: ArtworkCardProps) {
+    return (
+        <div className="artwork-card" onClick={onClick}>
+            <div className="artwork-card-container">
+                {/* 이미지 */}
+                <div className="artwork-image-container">
+                    <div
+                        className="artwork-image"
+                        style={{ backgroundImage: `url('${imageUrl}')` }}
+                    />
+                </div>
+
+                {/* 카드 정보 */}
+                <div className="artwork-info">
+                    <h3 className="artwork-title">
+                        {title}
+                    </h3>
+
+                    <div className="artwork-meta">
+                        <div className="artwork-views">
+                            <img src={eye_icon} style={{
+                                width: '12px',
+                            }} alt="" />
+                            <span>{viewCount}</span>
+                        </div>
+                        {createdDate && (
+                            <span className="artwork-date">{createdDate}</span>
+                        )}
+                    </div>
+                </div>
+                <div className="custom-checkbox-container">
+                    {isSelected && (
+                        <CustomCheckbox
+                            id={title}
+                            visible={isSelected}
+                            onToggleVisible={onClick}
+                            size={24}
+                        />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
