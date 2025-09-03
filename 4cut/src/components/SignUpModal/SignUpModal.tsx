adc8375e08e7c04b087458201030d4b39b9dd87e
jisu-d <lim16111611@gmail.com>
2025-09-04 00:32:51 +0900

    주석 삭제함.

--- 4cut/src/components/SignUpModal/SignUpModal.tsx
+++ 4cut/src/components/SignUpModal/SignUpModal.tsx
@@ -76,9 +76,6 @@ export default function SignupModal({ isOpen, onClose }: SignupModalProps) {
         <div className="signup-modal-header">
           <h2>회원가입</h2>
           <button className="close-button" onClick={onClose}>
-            {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg> */}
             <Cross />
           </button>
         </div>
