649154e8fefa4ca19d1a25ef7ed0cc5480686f00
jisu-d <lim16111611@gmail.com>
2025-09-04 00:32:03 +0900

    사진촬영 컴포넌트 PhotoCapture layout 생성

---
+++ 4cut/src/pages/PhotoShootPage.tsx
@@ -0,0 +1,12 @@
+
+import PhotoCapture from "../components/PhotoShoot/PhotoCapture/PhotoCapture.tsx";
+import "../styles/PhotoShoot/PhotoShootPage.css";
+
+function PhotoShootPage() {
+  return (
+      //<PhotoCapture aspectRatio={4 / 3} />
+      <PhotoCapture />
+  );
+}
+
+export default PhotoShootPage;
