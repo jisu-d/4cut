75c787106610e975a4d5f1088ecf91efb455088f
jisu-d <lim16111611@gmail.com>
2025-12-28 19:14:45 +0900

    ImageDownloadPage 없던거 수정함

--- 4cut/src/App.tsx
+++ 4cut/src/App.tsx
@@ -4,6 +4,7 @@ import Workspace from './pages/Workspace';
 import SignUp from './pages/SignUp';
 import Mypage from './pages/Mypage';
 import PhotoShootPage from './pages/PhotoShootPage.tsx';
+import ImageDownloadPage from './pages/ImageDownloadPage.tsx';
 import Test from './pages/Test';
 import { useEffect } from 'react';
 
@@ -29,14 +30,15 @@ function App() {
 
   return (
       <Routes>
-        <Route path="/MainPage" element={<MainPage />} />
-        <Route path="/Workspace" element={<Workspace />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Mypage" element={<Mypage />} />
-        <Route path="/PhotoShoot" element={<PhotoShootPage />} />
-        <Route path="/Test" element={<Test />} />
-        {/* 루트 경로 ('/')로 접근했을 때 PhotoShootPage로 리디렉션 */}
-        <Route path="/" element={<PhotoShootPage />} />
+          <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/Workspace" element={<Workspace />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Mypage" element={<Mypage />} />
+          <Route path="/PhotoShoot" element={<PhotoShootPage />} />
+          <Route path="/ImageDownloadPage" element={<ImageDownloadPage />} />
+          <Route path="/Test" element={<Test />} />
+          {/* 루트 경로 ('/')로 접근했을 때 PhotoShootPage로 리디렉션 */}
+          <Route path="/" element={<PhotoShootPage />} />
       </Routes>
   );
 }
